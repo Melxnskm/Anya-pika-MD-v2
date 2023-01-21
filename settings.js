d2d118757c3904fec6236e22326dfb966109c711
Medoggg <118803530+Medoggg@users.noreply.github.com>
2023-01-21 05:36:15 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,39 +2,39 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.botname = "Queen Anya" 
+global.botname = "Shanks" 
 global.botLanguage = "default" // 𝗗𝗲𝗳𝗮𝘂𝗹𝘁 || 𝗘𝗻𝗴𝗹𝗶𝘀𝗵 || 𝗛𝗶𝗻𝗱𝗶 || 𝗔𝗿𝗮𝗯𝗶𝗰 \\
-global.author = "𝙋𝙄𝙆𝘼"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "https://github.com/PikaBotz" // You can put any link 
+global.author = "Medo"  
+global.packname = "🔱...𝑴𝑬𝑫𝑶..🐉"  
+global.myweb = "No Link" // You can put any link 
 global.waterMark = "×͜× King ✘ Pika™ ✓" // Your own watermark 
-global.headerText = "Anya by Pika" // text for header of menu         
-global.DocumentTitle = "MASTER⚔️PIKA" 
+global.headerText = "🔱...𝑴𝑬𝑫𝑶..🐉" // text for header of menu         
+global.DocumentTitle = "🔱...𝑴𝑬𝑫𝑶..🐉" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
-global.ytname = "YT: Ytname"   // Yt chanel name //
-global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
+global.ownername = "🔱...𝑴𝑬𝑫𝑶..🐉"   // Owner name //        
+global.ownernumber = ['201016892396'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 :  privat" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ytname = "YT: 😵"   // Yt chanel name //
+global.continent = "Africa" // your continent name 
+global.region = "Egypt" // your country name
+global.state = "private" // your state name
 global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
-global.email = "example@exmaple.com" // put your email here
+global.timezone = "Africa/Cairo" // search on google if you don't know the timezone of your country //
+global.instagramId = "private" // Your insta ID
+global.email = "mm0899701@gmail.com" // put your email here
 
 
 //------------ 🌟 Premium Users 🌟-------------\\
-global.premium = ['916900******','917355******']  // available soon
-global.sudo = ['916900******','917355******']  // Co - Owners //
+global.premium = ['201016892396','201508610020']  // available soon
+global.sudo = ['201016892396','201508610020']  // Co - Owners //
 
-global.prefa = ['-','🥵','🔥']
+global.prefa = ['-','🗿','🔥']
 
 //--------Random bot name for Info section--------\\
-name16 = "𓄂⍣⃝ 🇦𝚗𝚢𝚊 𓆩×͜×𓆪ꪾ️"
-name17 = "*Λ⃝ͷ🇾Λ 𓆩×͜×𓆪ꪾ*"
-name18 = "*Λ⃝ͷʏΛ 𓆩×͜×𓆪ꪾ*"
+name16 = "S̷h̷a̷n̷k̷s̷ 𓆩×͜×𓆪ꪾ️"
+name17 = "S̷h̷a̷n̷k̷s̷ 𓆩×͜×𓆪ꪾ*"
+name18 = "S̷h̷a̷n̷k̷s̷ 𓆩×͜×𓆪ꪾ*"
 
 //--------Random footer message-----------\\
 footer1 = `𓄂⍣⃝🇦𝚗𝚢𝚊ㅤꪜ⃢𝟤ㅤ𓆩×͜×𓆪ꪾ `
